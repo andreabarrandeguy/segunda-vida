0e4b69f6ee43d8f430c10be99f6233d7bb6a6086
andreabarrandeguy <abarrandeguy87@hotmail.com>
2022-04-19 21:21:38 -0300

    Carga 19 de Abril

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const path = require('path')
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
